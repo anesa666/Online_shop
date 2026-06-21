e34f6626f1a3cf21b3b3d30a3f33b5f2a0e6bade
anesa666 <anesahoxha81@gmail.com>
2026-06-21 21:01:49 +0200

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
 import Navbar from './components/Navbar.jsx';
-import './index.css'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function App() {
-  return <Navbar />
+  return <div>
+    <Navbar/>
+  </div>
 }
-
-
-//test data
-const MOCK_PRODUCTS = [
-  { id: 1, name: "Blue T-Shirt", price: 19.99, image_url: "https://via.placeholder.com/150", stock: 10 },
-  { id: 2, name: "Black Hoodie", price: 45.50, image_url: "https://via.placeholder.com/150", stock: 3 },
-  { id: 3, name: "White Sneakers", price: 80.00, image_url: "https://via.placeholder.com/150", stock: 0 }
-];
 
 export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa6";
+
 
 function Navbar(){
+
+  const[isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = ()=>{
+    setIsOpen(!isOpen)
+  };
+
   return (
-    <div>Navbar</div>
+    <>
+    <header>
+      <div className="container">
+      <nav>
+        <div className="logo">
+        <h2>
+      STORE NAME
+      </h2>
+      </div>
+      <ul className={isOpen ? "nav-link active" : "nav-link"}>
+        <li><a href="/home" className="active">Home</a></li>
+        <li><a href="/products">Products</a></li>
+        <li><a href="/categories">Categories</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <div className="icon" onClick={toggleMenu}>
+        <FaBars />
+      </div>
+      </nav>
+    </div>
+    </header>
+    </>
   )
 }
 
